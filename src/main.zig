0d6012d210a1c189ea108d1b3412d7dd0aa3efb1
Ali Chraghi <alichraghi@pm.me>
2023-03-01 13:23:23 +0330

    s

--- src/main.zig
+++ src/main.zig
@@ -7,5 +7,8 @@ test {
     std.testing.refAllDeclsRecursive(Token);
     std.testing.refAllDeclsRecursive(Tokenizer);
     std.testing.refAllDeclsRecursive(Ast);
+    std.testing.refAllDeclsRecursive(@import("ErrorList.zig"));
+    std.testing.refAllDeclsRecursive(@import("TokenList.zig"));
     std.testing.refAllDeclsRecursive(@import("Parser.zig"));
+    std.testing.refAllDeclsRecursive(@import("resolve.zig"));
 }

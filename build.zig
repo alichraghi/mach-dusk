d2dab4f1ade25d33aa8cdf57660d26314bb87277
Ali Chraghi <alichraghi@pm.me>
2023-03-01 15:31:24 +0330

    t

--- build.zig
+++ build.zig
@@ -4,11 +4,16 @@ pub fn build(b: *std.build.Builder) void {
     const optimize = b.standardOptimizeOption(.{});
     const main_tests = b.addTest(.{
         .kind = .test_exe,
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = .{ .path = "test/main.zig" },
         .optimize = optimize,
     });
+    main_tests.addModule("dusk", module(b));
     main_tests.linkLibC();
     main_tests.install();
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.run().step);
+}
+
+pub fn module(b: *std.build.Builder) *std.build.Module {
+    return b.createModule(.{ .source_file = .{ .path = "src/main.zig" } });
 }
--- src/main.zig
+++ src/main.zig
@@ -1,14 +1,3 @@
 pub const Token = @import("Token.zig");
 pub const Tokenizer = @import("Tokenizer.zig");
 pub const Ast = @import("Ast.zig");
-
-const std = @import("std");
-test {
-    std.testing.refAllDeclsRecursive(Token);
-    std.testing.refAllDeclsRecursive(Tokenizer);
-    std.testing.refAllDeclsRecursive(Ast);
-    std.testing.refAllDeclsRecursive(@import("ErrorList.zig"));
-    std.testing.refAllDeclsRecursive(@import("TokenList.zig"));
-    std.testing.refAllDeclsRecursive(@import("Parser.zig"));
-    std.testing.refAllDeclsRecursive(@import("resolve.zig"));
-}
